2a2850081e732925539f27700449ea9a72a676f3
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-07 12:00:00 +0300

    added: index.js; created a function 'test'

---
+++ index.js
@@ -0,0 +1,12 @@
+const test = (states, str) => {
+  const queue = states.slice();
+  let i = 0;
+  let currentState = queue.shift();
+
+  while (currentState) {
+    switch (currentState.quantifier) {
+      case "exactlyOne": {
+      }
+    }
+  }
+};
