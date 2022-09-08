d4e24290986de45ecd491e8a5f186591ff6f10ed
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-08 20:56:22 +0300

    index.js: created a function 'stateMatchesStringAtIndex'

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const test = (states, str) => {
+const stateMatchesStringAtIndex = (state, str, i) => {
+  if (i >= str.length) {
+    return [false, 0];
+  }
+  if (state.type === "wildcard") {
+    return [true, 1];
+  }
+  if (state.type === "element") {
+    const match = state.value === str[i];
+    return [match, match ? 1 : 0];
+  }
+  if (state.type === "groupElement") {
+    return test(state.states, str.slice(i));
+  }
+  throw new Error("Unsupported element type");
+};
+
+function test(states, str) {
   const queue = states.slice();
   let i = 0;
   let currentState = queue.shift();
@@ -9,4 +26,4 @@ const test = (states, str) => {
       }
     }
   }
-};
+}
