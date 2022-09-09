28ace4435162a06b8535c1f8206ef764022575d0
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-09 23:43:09 +0300

    added: conditions in the function 'test'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const parse = require("./parser.js");
+
 const stateMatchesStringAtIndex = (state, str, i) => {
   if (i >= str.length) {
     return [false, 0];
@@ -23,7 +25,58 @@ function test(states, str) {
   while (currentState) {
     switch (currentState.quantifier) {
       case "exactlyOne": {
+        [isMatch, consumed] = stateMatchesStringAtIndex(currentState, str, i);
+
+        if (!isMatch) {
+          return [false, i];
+        }
+        i += consumed;
+        currentState = queue.shift();
+        continue;
+      }
+      case "zeroOrOne": {
+        if (i >= str.length) {
+          currentState = queue.shift();
+          continue;
+        }
+        const [isMatch, consumed] = stateMatchesStringAtIndex(
+          currentState,
+          str,
+          i
+        );
+        i += consumed;
+        currentState = queue.shift();
+        continue;
+      }
+      case "zeroOrMore": {
+        while (true) {
+          if (i >= str.length) {
+            currentState = queue.shift();
+            break;
+          }
+          const [isMatch, consumed] = stateMatchesStringAtIndex(
+            currentState,
+            str,
+            i
+          );
+          if (!isMatch || consumed === 0) {
+            currentState = queue.shift();
+            break;
+          }
+          i += consumed;
+        }
+        continue;
+      }
+      default: {
+        throw new Error("Unsupported operation");
       }
     }
   }
+  return [true, i];
 }
+
+const regex = "a(b.)*cd";
+const states = parse(regex);
+const exampleStr = "ab!b$cd";
+const result = test(states, exampleStr);
+console.log(result);
